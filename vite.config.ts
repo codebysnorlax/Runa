7d3c6a00b5d065346447d3b81bf4b8ef7727f716
Ravi Ranjan Sharma <rr2436310@gmail.com>
2025-11-17 23:34:45 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: process.env.VITE_BASE_PATH || '/',
       server: {
         port: 3001,
         host: '0.0.0.0',
@@ -21,5 +22,4 @@ export default defineConfig(({ mode }) => {
         }
       }
     };
-    base: process.env.VITE_BASE_PATH || '/',
 });
